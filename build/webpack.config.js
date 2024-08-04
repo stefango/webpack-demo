b4d0e9bed8f131397ddd94881ec5029a8f56c085
stefango <nankongyinan@gmail.com>
2024-08-04 23:01:11 +0800

    feat: asset-management

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,9 +1,33 @@
 const path = require('path');
+const yaml = require('yamljs');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, '../dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.yaml$/i,
+        type: 'json',
+        parser: {
+          parse: yaml.parse,
+        },
+      },
+    ],
   },
 };
