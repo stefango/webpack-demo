71e8689f6eb7b2fc80ad80c87c33b22ddc5180a9
stefango <nankongyinan@gmail.com>
2024-11-15 19:52:22 +0800

    feat: add react/scss/eslint

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+// todo eslint format
+import
+  styles from './index.module.scss'
+
+export function Index() {
+  return (
+    <div
+      className={styles.moduleTest}
+      id="12"
+    >
+       {/* todo eslint format */}
+      react app
+    </div>
+  );
+}
