d4ecb73bd41fba6ad925bfc5c9ba37527607048c
stefango <nankongyinan@gmail.com>
2025-06-23 20:56:38 +0800

    fix: update babel config

--- babel.config.js
+++ babel.config.js
@@ -3,9 +3,15 @@ const isDevelopment = process.env.NODE_ENV === 'development';
 module.exports = {
   presets: [
     '@babel/preset-env',
-    ['@babel/preset-react', {
-      runtime: 'automatic',
-    }],
+    [
+      '@babel/preset-react',
+      {
+        runtime: 'automatic',
+      },
+    ],
+    '@babel/preset-typescript',
   ],
-  plugins: [isDevelopment && require.resolve('react-refresh/babel')].filter(Boolean),
+  plugins: [isDevelopment && require.resolve('react-refresh/babel')].filter(
+    Boolean,
+  ),
 };
